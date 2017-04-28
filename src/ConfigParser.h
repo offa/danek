7b913df8bd946312a446e06a7ad3afe8d00b3752
offa <bm-dev@yandex.com>
2017-04-28 23:38:19 +0200

    License updated.

--- src/ConfigParser.h
+++ src/ConfigParser.h
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------
+// Copyright (c) 2017 offa
 // Copyright 2011 Ciaran McHale.
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -20,9 +20,7 @@
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-//
-//-----------------------------------------------------------------------
-//
+
 // BNF of config file
 // ------------------
 // Note:	"|" denotes choice
--- src/SchemaTypeList.cpp
+++ src/SchemaTypeList.cpp
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------
+// Copyright (c) 2017 offa
 // Copyright 2011 Ciaran McHale.
 //
 // Permission is hereby granted, free of charge, to any person obtaining
@@ -20,7 +20,6 @@
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-//----------------------------------------------------------------------
 
 #include "SchemaTypeList.h"
 #include "Common.h"
