3126104f047b9db69ca58e6456f270ae6ca63c81
offa <bm-dev@yandex.com>
2017-04-30 23:50:50 +0200

    Formatting.

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -4,9 +4,9 @@ find_program(CLANG_FORMAT_EXE clang-format DOC "Clang Format executable")
 if( CLANG_FORMAT_EXE )
     set(FORMAT_SOURCE_DIRS demos include src test tests)
 
-    add_custom_target(format 
-        find ${FORMAT_SOURCE_DIRS} -name "*.h" -o -name "*.cpp" | xargs
-        ${CLANG_FORMAT_EXE} -i ${FORMAT_SOURCES}
+    add_custom_target(format
+                        find ${FORMAT_SOURCE_DIRS} -name "*.h" -o -name "*.cpp" | xargs
+                            ${CLANG_FORMAT_EXE} -i ${FORMAT_SOURCES}
                         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                         COMMENT "Formatting Code"
                         VERBATIM
