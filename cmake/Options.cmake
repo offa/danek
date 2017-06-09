6cca73fc3d87b535c50d3ecdfc6df93cebcda152
offa <bm-dev@yandex.com>
2017-06-09 18:08:01 +0200

    Macro renamed.

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -6,16 +6,16 @@ else()
 endif()
 
 
-macro(_print_option _option _name _value _docstring)
+macro(_add_option _option _name _value _docstring)
     option(${_option} "${_docstring}" ${${_value}})
     message(STATUS "${_name} : ${${_option}}")
 endmacro()
 
 
-_print_option(UNIT_TESTS "Unit Tests" ON "Build Unit Tests")
-_print_option(COVERAGE "Coverage" OFF "Enable Coverage")
-_print_option(BUILD_TOOLS "Build Tools" ON "Build the tools")
-_print_option(BUILD_SCHEMA_TESTS "Build Schema Tests" OFF "Build the Schema Tests")
-_print_option(BUILD_DEMOS "Build Demos" OFF "Build the Demos")
-_print_option(BUILD_SHARED_LIBS "Build shared library" OFF "Build Shared Library")
+_add_option(UNIT_TESTS "Unit Tests" ON "Build Unit Tests")
+_add_option(COVERAGE "Coverage" OFF "Enable Coverage")
+_add_option(BUILD_TOOLS "Build Tools" ON "Build the tools")
+_add_option(BUILD_SCHEMA_TESTS "Build Schema Tests" OFF "Build the Schema Tests")
+_add_option(BUILD_DEMOS "Build Demos" OFF "Build the Demos")
+_add_option(BUILD_SHARED_LIBS "Build shared library" OFF "Build Shared Library")
 
