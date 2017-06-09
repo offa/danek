aa0087eb422c276be2734afd54af9c3a0f1a6cb3
offa <bm-dev@yandex.com>
2017-06-09 17:41:16 +0200

    Option names unified (fixes #68).

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -4,7 +4,7 @@ set -ex
 
 mkdir build && cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DDEMOS=ON -DSCHEMA_TESTS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DEMOS=ON -DBUILD_SCHEMA_TESTS=ON ..
 make
 make unittest
 
