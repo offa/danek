dd4d2bf0313692a5671a363d35e1d4a005b43e85
offa <bm-dev@yandex.com>
2017-09-22 15:01:48 +0200

    Better Cmake messages.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -2,7 +2,7 @@ include(CheckCXXCompilerFlag)
 
 function(add_compile_options_if_supported)
     foreach(flag ${ARGN})
-        set(option_var option_supported${flag})
+        set(option_var option_supported_${flag})
         CHECK_CXX_COMPILER_FLAG(${flag} ${option_var})
 
         if( ${option_var} )
