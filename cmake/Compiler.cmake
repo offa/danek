03403bce066e538d81be906837b34ddbbe63a9d5
offa <bm-dev@yandex.com>
2017-09-22 13:56:44 +0200

    Refactoring.

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,3 +1,15 @@
+include(CheckCXXCompilerFlag)
+
+function(add_compile_options_if_supported)
+    foreach(flag ${ARGN})
+        set(option_var option_supported${flag})
+        CHECK_CXX_COMPILER_FLAG(${flag} ${option_var})
+
+        if( ${option_var} )
+            add_compile_options(${flag})
+        endif()
+    endforeach()
+endfunction()
 
 macro(add_c_flag)
     foreach(flag ${ARGN})
