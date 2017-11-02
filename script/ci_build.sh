da2ba9aefcdbea7893b9554fa746d9af520cdc42
offa <bm-dev@yandex.com>
2017-11-02 19:47:15 +0100

    Cleanup.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
+
+if [[ "$CXX" == clang* ]]
+then
+    export CXXFLAGS="-stdlib=libc++"
+fi
+
+
 mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DEMOS=ON -DBUILD_SCHEMA_TESTS=ON ..
