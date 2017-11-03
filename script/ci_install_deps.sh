0d0586e5d50fe6f5d783e32d66d24cc3f67b45a7
offa <bm-dev@yandex.com>
2017-11-03 16:29:59 +0100

    Cleanup.

--- script/ci_install_deps.sh
+++ script/ci_install_deps.sh
@@ -20,19 +20,8 @@ if [[ "${CXX}" == clang* ]]
 then
     BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_CXX_FLAGS=-stdlib=libc++"
 
-
-    echo "*** Searching for cxxabi.h ***"
-    find /usr/ -type f -name "cxxabi.h"
-    echo "****"
-    ls /usr/local/clang-3.9.0/include/c++/v1
-    echo "++++++++++++++"
-    find ${DEPENDENCY_DIR} -type f -name "cxxabi.h"
-
     sudo ln -s ${DEPENDENCY_DIR}/llvm-source/projects/libcxxabi/include/cxxabi.h /usr/include/cxxabi.h
-    ls ${DEPENDENCY_DIR}/llvm-source/projects/libcxxabi/include/
     sudo ln -s ${DEPENDENCY_DIR}/llvm-source/projects/libcxxabi/include/__cxxabi_config.h /usr/include/__cxxabi_config.h
-    ls -l /usr/include/
-
 fi
 
 mkdir -p build-${CC} && cd build-${CC}
