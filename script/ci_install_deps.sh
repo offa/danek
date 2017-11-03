5a32987b1d4bd482284e6daade7c445841a9efe4
offa <bm-dev@yandex.com>
2017-11-03 15:56:35 +0100

    Further include paths which contains the necessary file.

--- script/ci_install_deps.sh
+++ script/ci_install_deps.sh
@@ -18,11 +18,13 @@ cd googletest
 
 if [[ "${CXX}" == clang* ]]
 then
-    BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_CXX_FLAGS=-stdlib=libc++ -I/usr/local/clang-3.9.0/include/c++/v1/"
+    BUILD_FLAGS="${BUILD_FLAGS} -DCMAKE_CXX_FLAGS=-stdlib=libc++ -I/usr/local/clang-3.9.0/include/c++/v1 -I/usr/include/c++/4.8"
 
 
     echo "*** Searching for cxxabi.h ***"
     find /usr/ -type f -name "cxxabi.h"
+    echo "****"
+    ls /usr/local/clang-3.9.0/include/c++/v1
 fi
 
 mkdir -p build-${CC} && cd build-${CC}
