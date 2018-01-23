77e51235caff483175105eab264c32d87b9ca3a0
offa <bm-dev@yandex.com>
2018-01-23 12:42:28 +0100

    Handling of GTest improved (fixes #107).

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,5 +1,7 @@
-
 find_package(PkgConfig)
+find_package(Threads REQUIRED)
+
+
 pkg_check_modules(PKG_GTest QUIET libGTest)
 set(GTest_DEFINITIONS ${PKG_GTest_CFLAGS_OTHER})
 
@@ -10,29 +12,57 @@ find_path(GTest_INCLUDE_DIR "gtest/gtest.h"
 find_path(GTest_Mock_INCLUDE_DIR "gmock/gmock.h"
                                 HINTS ${PKG_GMock_INCLUDE_DIRS}
                                         "${GMock_DIR}/include"
+                                        ${GTest_INCLUDE_DIR}
                                 )
 
-find_library(GTest_LIBRARY NAMES gmock
+find_library(GTest_LIBRARY NAMES gtest
                                 HINTS ${PKG_GTest_LIBDIR}
                                         ${PKG_GTest_LIBRARY_DIRS}
                                 )
-find_library(GTest_Main_LIBRARY NAMES gmock_main
+find_library(GTest_Mock_LIBRARY NAMES gmock
                                 HINTS ${PKG_GTest_LIBDIR}
                                         ${PKG_GTest_LIBRARY_DIRS}
-                                        ${GTest_LIBRARY}/..
+                                )
+find_library(GTest_Main_LIBRARY NAMES gtest_main
+                                HINTS ${PKG_GTest_LIBDIR}
+                                        ${PKG_GTest_LIBRARY_DIRS}
                                 )
 
-
-set(GTest_LIBRARIES ${GTest_LIBRARY} ${GTest_Main_LIBRARY})
-set(GTest_INCLUDE_DIRS ${GTest_INCLUDE_DIR} ${GTest_Mock_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GTest DEFAULT_MSG
-                                        GTest_LIBRARY
-                                        GTest_Main_LIBRARY
-                                        GTest_INCLUDE_DIR
-                                        GTest_Mock_INCLUDE_DIR
-                                        )
+                                    GTest_INCLUDE_DIR
+                                    GTest_Mock_INCLUDE_DIR
+                                    GTest_LIBRARY
+                                    GTest_Mock_LIBRARY
+                                    GTest_Main_LIBRARY
+                                    )
+mark_as_advanced(GTest_INCLUDE_DIR
+                GTest_Mock_INCLUDE_DIR
+                GTest_LIBRARY
+                GTest_Mock_LIBRARY
+                GTest_Main_LIBRARY
+                )
 
-mark_as_advanced(GTest_INCLUDE_DIR GTest_Mock_INCLUDE_DIR GTest_LIBRARY GTest_Main_LIBRARY)
+
+add_library(GTest::GTest UNKNOWN IMPORTED)
+set_target_properties(GTest::GTest PROPERTIES
+                        IMPORTED_LOCATION "${GTest_LIBRARY}"
+                        IMPORTED_LINK_INTERFACE_LANGUAGES CXX
+                        INTERFACE_INCLUDE_DIRECTORIES "${GTest_INCLUDE_DIR}"
+                        INTERFACE_LINK_LIBRARIES Threads::Threads
+                        )
+
+add_library(GTest::Main UNKNOWN IMPORTED)
+set_target_properties(GTest::Main PROPERTIES
+                        IMPORTED_LOCATION "${GTest_Main_LIBRARY}"
+                        IMPORTED_LINK_INTERFACE_LANGUAGES CXX
+                        )
+
+add_library(GTest::Mock UNKNOWN IMPORTED)
+set_target_properties(GTest::Mock PROPERTIES
+                        IMPORTED_LOCATION "${GTest_Mock_LIBRARY}"
+                        IMPORTED_LINK_INTERFACE_LANGUAGES CXX
+                        INTERFACE_INCLUDE_DIRECTORIES "${GTest_Mock_INCLUDE_DIR}"
+                        )
 
