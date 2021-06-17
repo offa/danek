20f7af0cc49b34144a19e92163c2e549041d93a7
offa <bm-dev@yandex.com>
2021-06-17 18:17:57 +0200

    Enable GTest brief output on CI builds.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -8,10 +8,10 @@ then
     export CXXFLAGS="-stdlib=libc++"
 fi
 
+export GTEST_BRIEF=1
 
 mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DEMOS=ON -DBUILD_SCHEMA_TESTS=ON ..
 make
 make unittest
-
