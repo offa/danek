fbd3196bed8bfbe1883139b3875579b281ec9b48
offa <bm-dev@yandex.com>
2021-06-17 18:22:20 +0200

    Use Conan for dependency management (#128).

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -3,14 +3,26 @@
 set -ex
 
 
+# Conan
+apt-get install -y python3-pip
+pip3 install -U conan
+conan profile new default --detect
+
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
+    conan profile update settings.compiler.libcxx=libc++ default
+else
+    conan profile update settings.compiler.libcxx=libstdc++11 default
 fi
 
-export GTEST_BRIEF=1
 
 mkdir build && cd build
+
+conan install --build=missing ..
+
+
+export GTEST_BRIEF=1
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DEMOS=ON -DBUILD_SCHEMA_TESTS=ON ..
 make
