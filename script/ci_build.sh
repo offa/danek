57b6e1b6b96e673cbdf09f797c95bdf9efc721b9
offa <bm-dev@yandex.com>
2023-02-23 18:05:14 +0100

    Migrate to Conan v2.0 (#134)

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,29 +2,38 @@
 
 set -ex
 
+BUILD_TYPE="Release"
 
 # Conan
 apt-get update
 apt-get install -y python3-pip
 pip3 install -U conan
-conan profile new default --detect
+conan profile detect
 
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
-    conan profile update settings.compiler.libcxx=libc++ default
-else
-    conan profile update settings.compiler.libcxx=libstdc++11 default
+    sed -i 's/^compiler.libcxx=.*$/compiler.libcxx=libc++/g' ~/.conan2/profiles/default
 fi
 
 
 mkdir build && cd build
 
-conan install --build=missing ..
+conan install \
+    --build=missing \
+    -s build_type=${BUILD_TYPE} \
+    ..
 
 
 export GTEST_BRIEF=1
 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DEMOS=ON -DBUILD_SCHEMA_TESTS=ON ..
+
+cmake -DCMAKE_TOOLCHAIN_FILE=./conan_toolchain.cmake -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" "${BUILD_ARGS[@]}" ..
+
+cmake -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+    -DCMAKE_TOOLCHAIN_FILE=./conan_toolchain.cmake \
+    -DBUILD_DEMOS=ON \
+    -DBUILD_SCHEMA_TESTS=ON \
+    ..
 make
 make unittest
